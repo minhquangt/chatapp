c7ad053c456ec45caf26294a6bc3fd9905f31501
Quang Trần Minh <qtranminh77@gmail.com>
2023-08-27 11:18:43 +0700

    init project

---
+++ src/config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCrClEtDd6DLy55HRz4nvN-gvou2uSzl9g',
+    authDomain: 'chatapp-811f8.firebaseapp.com',
+    projectId: 'chatapp-811f8',
+    storageBucket: 'chatapp-811f8.appspot.com',
+    messagingSenderId: '812140467596',
+    appId: '1:812140467596:web:84f5b549a500c9984bf2a9',
+};
+
+const app = initializeApp(firebaseConfig);
